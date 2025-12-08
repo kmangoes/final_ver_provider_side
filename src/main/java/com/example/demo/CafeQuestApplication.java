7cf08138035082c43b547be54eeeb60474176ecb
kmangoes <sp24lights@gmail.com>
2025-12-07 23:36:32 -0500

    updates

--- src/main/java/com/example/demo/CafeQuestApplication.java
+++ src/main/java/com/example/demo/CafeQuestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalProviderSideApplication {
+public class CafeQuestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinalProviderSideApplication.class, args);
+		SpringApplication.run(CafeQuestApplication.class, args);
 	}
 
 }
